a345387d1fb47a7bb65db6bf10d31658d80098e5
BryanHo10 <bryankho1010@gmail.com>
2020-03-13 09:55:22 -0700

    Implement more User API functions

--- User/userAPI.py
+++ User/userAPI.py
@@ -22,34 +22,50 @@ def delete_user(userid):
 	conn = sqlite3.connect('example.db')
 	cur = conn.cursor()
 	sql = 'DELETE FROM user WHERE userid=?'
-    cur.execute(sql, (userid,))
+    cur.execute(sql, (userid))
     conn.commit()
 
     return
 
 def update_email(userid,email_addr):
+    conn = sqlite3.connect('example.db')
+	cur = conn.cursor()
+    cur.execute("UPDATE user SET email=? WHERE userid=?", (email_addr,userid))
+	conn.commit()
+	conn.close()
     return
 
 def get_user(userid):
+    conn = sqlite3.connect('example.db')
+	cur = conn.cursor()
+    cur.execute("SELECT * user WHERE userid=?", (userid))
+	conn.commit()
+	conn.close()
     return
 
 def inc_karma(userid):
+    
     return
 
 def dec_karma(userid):
     return
 
-def create_user(username,email,password):
+def create_user(username_in,email_in,password_in):
 	conn = sqlite3.connect('example.db')
 	cur = conn.cursor()
 	cur.execute('''CREATE TABLE IF NOT EXISTS user (
     			username text PRIMARY KEY,
     			email text NOT NULL,
     			password text NOT NULL)''')
-    cur.execute("INSERT INTO user VALUES (username, email, password)")
+    cur.execute("INSERT INTO user VALUES (?, ?, ?)",(username_in,email_in,password_in))
 	conn.commit()
 	conn.close()
     return
 
 def get_all_users():
+    conn = sqlite3.connect('example.db')
+	cur = conn.cursor()
+    cur.execute("SELECT * user")
+	conn.commit()
+	conn.close()
     return
