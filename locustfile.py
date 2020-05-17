70bc83cd3dc74d38106ae541b8268a17d8c9459d
CMier <caesarmier@csu.fullerton.edu>
2020-05-17 09:43:21 -0700

    Add files via upload

--- locustfile.py
+++ locustfile.py
@@ -10,9 +10,6 @@ class MyTasks(TaskSet):
     def get_user_profile(self):
         self.client.get("/reddit-mock/api/v1.0/user")
 
-    @task(1)
-    def put_user_profile_error(self):
-        self.client.put("/reddit-mock/api/v1.0/user")
 
 
 
@@ -21,18 +18,13 @@ class MyTasks(TaskSet):
     def inc_karma_put(self):
         self.client.put("/reddit-mock/api/v1.0/user/2/karma/increment")
 
-    @task(1)
-    def inc_karma_error_post(self):
-        self.client.post("/reddit-mock/api/v1.0/user/2/karma/increment")
+    
 
     @task(1)
     def dec_karma_put(self):
         self.client.put("/reddit-mock/api/v1.0/user/2/karma/decrement")
 
-    @task(1)
-    def dec_karma_error_post(self):
-        self.client.post("/reddit-mock/api/v1.0/user/2/karma/decrement")
-
+   
 
 
 
@@ -48,27 +40,14 @@ class MyTasks(TaskSet):
     def get_user(self):
         self.client.get("/reddit-mock/api/v1.0/user/2")
 
-    @task(1)
-    def error_post_user(self):
-        self.client.post("/reddit-mock/api/v1.0/user/2")
-
-
-
-
-
-
-
-
+   
 
     @task(5)
     def create_post(self):
         self.client.post("/reddit-mock/api/v1.0/user/2/post", {"title": "First!", "message": "This is my first message",
                                                                "communityID": "load1"})
 
-    @task(1)
-    def create_post_get_error(self):
-        self.client.get("/reddit-mock/api/v1.0/user/2/post", {"title": "First!", "message": "This is my first message",
-                                                               "communityID": "load1"})
+    
 
     @task(1)
     def delete_user_post(self):
@@ -78,25 +57,57 @@ class MyTasks(TaskSet):
     def get_user_post(self):
         self.client.get("/reddit-mock/api/v1.0/user/2/post/1")
 
-    @task(1)
-    def put_user_post_error(self):
-        self.client.put("/reddit-mock/api/v1.0/user/2/post/1")
+    
 
     @task(1)
     def get_list_posts(self):
         self.client.get("/reddit-mock/api/v1.0/post/2")
 
-    @task(1)
-    def put_list_posts_error(self):
-        self.client.put("/reddit-mock/api/v1.0/post/2")
+   
 
     @task(1)
     def get_community_list_posts(self):
         self.client.get("/reddit-mock/api/v1.0/post/cpsc/2")
 
+
     @task(1)
-    def put_community_list_posts_error(self):
-        self.client.put("/reddit-mock/api/v1.0/post/cpsc/2")
+    def create_message(self):
+        self.client.post("/reddit-mock/api/v1.0/user/2/message")
+
+    @task(1)
+    def get_user_message(self):
+        self.client.get("/reddit-mock/api/v1.0/user/2/message")
+
+    @task(1)
+    def favorite_message(self):
+        self.client.put("/reddit-mock/api/v1.0/user/1/message/", {"messageID" : "428c8230-9af6-44bb-9912-ac78ff28ba9b"})
+
+    @task(1)
+    def delete_message(self):
+        self.client.delete("/reddit-mock/api/v1.0/user/1/message/", {"messageID" : "428c8230-9af6-44bb-9912-ac78ff28ba9b"})
+
+    @task(1)
+    def upvote_post(self):
+        self.client.put("/reddit-mock/api/v1.0/user/2/post/1/upvote")
+
+    @task(1)
+    def downvote_post(self):
+        self.client.put("/reddit-mock/api/v1.0/user/2/post/1/downvote")
+
+    @task(1)
+    def get_scores(self):
+        self.client.get("/reddit-mock/api/v1.0/post/1/vote")
+
+    @task(1)
+    def get_list_scores(self):
+        self.client.get("/reddit-mock/api/v1.0/post/vote/2")
+    @task(1)
+    def get_uniqe_post_score(self):
+        self.client.get("/reddit-mock/api/v1.0/post/vote")
+
+    
+
+    
 
 
 
